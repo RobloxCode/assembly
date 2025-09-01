72305fc355d2489adfd215d61bb9da5cca4762f3
Miguel <mloranca703@gmail.com>
2025-08-31 19:56:44 -0600

    learned about sections and labels

--- hello_world/hello.asm
+++ hello_world/hello.asm
@@ -1,17 +1,38 @@
 section .data
-    text db "Hello world!", 10
+    greater db "is greater", 10
+    smaller db "is smaller", 10
 
 section .text
     global _start
+    global print_greater
+    global print_smaller
+    global return_sc
 
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, text
-    mov rdx, 14
-    syscall
-
+return_sc:
     mov rax, 60
     mov rdi, 0
     syscall
+
+print_greater:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, greater
+    mov rdx, 11
+    syscall
+
+    jmp return_sc
+
+print_smaller:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, smaller
+    mov rdx, 11
+    syscall
+
+    jmp return_sc
+
+_start:
+    cmp rax, 2
+    jl print_smaller
+    jmp return_sc
 
