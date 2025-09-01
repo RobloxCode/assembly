5ec7c48789d088ff0cac5082e4ea60691ec81163
Miguel <mloranca703@gmail.com>
2025-08-31 20:12:29 -0600

    added the README description

---
+++ sections/section.asm
@@ -0,0 +1,33 @@
+; each section is a chunk of code where we will do
+; some specific tasks
+; for example, in the .data section we define our
+; variables
+section .data
+    msg db "this projects is about sections", 10
+
+; there's also a bss section where we define variables
+; for the future or something like that idk
+
+; in the .text section is where all of our code will 
+; be, and also we put our labels a label is a like 
+; a function, cuz when we call it we do a jump
+section .text
+    ; the _start label is like the main function/entry
+    ; of every assembly program
+    global _start
+    global success_exit         ; creating my own label
+
+success_exit:
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 32
+    syscall
+
+    jmp success_exit            ; jumping to the success_exit label
+
