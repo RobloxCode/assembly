f3f8d5d7d5bebe5427ebab04f3e765252acfd086
Miguel <mloranca703@gmail.com>
2025-09-11 19:26:12 -0600

    made the calculator interface

--- assembly_8086/exerc1/main.asm
+++ assembly_8086/exerc1/main.asm
@@ -5,18 +5,18 @@ carriage_ret_char equ 13
 .stack 100h
 
 .data
-    tittle db 'Shape calculator$', new_line_char, carriage_ret_char
-    shapes_list_msg db 'Cicle', new_line_char, carriage_ret_char, 'Cube', new_line_char, carriage_ret_char, 'Triangle', new_line_char, carriage_ret_char, dollar_sign_char
+    title_msg db 35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,new_line_char,carriage_ret_char,35, '     CALCULATOR     ',35,new_line_char,carriage_ret_char,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35
+    shapes_lst_msg db new_line_char, carriage_ret_char, 'Circle' ,10, 13, 'Cube', 10, 13, 'Triangle', 10, 13, 36
 
 .code
 start:
     mov ax, @data
     mov ds, ax
 
-    mov dx, offset tittle
+    mov dx, offset title_msg
     call _display_msg
 
-    mov dx, offset shapes_list_msg
+    mov dx, offset shapes_lst_msg
     call _display_msg
 
     call _exit
@@ -31,3 +31,4 @@ _exit proc
     mov ah, 4Ch
     int 21h
 _exit endp
+
