5a3cec201bb144e1118bafa20ea81fd2f27b1cb2
Miguel <mloranca703@gmail.com>
2025-09-13 17:26:18 -0600

    fixed the code for the calculator menu and shapes options

--- assembly_8086/exerc1/main.asm
+++ assembly_8086/exerc1/main.asm
@@ -1,12 +1,19 @@
 new_line_char equ 10
-dollar_sign_char equ 36
 carriage_ret_char equ 13
+dollar_sign_char equ 36
 
 .stack 100h
 
 .data
-    title_msg db 35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,new_line_char,carriage_ret_char,35, '     CALCULATOR     ',35,new_line_char,carriage_ret_char,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35
-    shapes_lst_msg db new_line_char, carriage_ret_char, 'Circle' ,10, 13, 'Cube', 10, 13, 'Triangle', 10, 13, 36
+    title_msg db 35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35
+              db 35,35,35,35,35,35,35,35,35,35,new_line_char,carriage_ret_char
+              db 35,'         CALCULADORA        ',35,new_line_char,carriage_ret_char
+              db 35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35
+              db 35,35,35,35,35,35,35,35,35,new_line_char,carriage_ret_char,dollar_sign_char
+
+    lst_shapes_msg db 49,' ',45,62,' Circulo',new_line_char,carriage_ret_char
+                   db 49,' ',45,62,' Cuadrado',new_line_char,carriage_ret_char
+                   db 49,' ',45,62,' Triangulo',new_line_char,carriage_ret_char,dollar_sign_char
 
 .code
 start:
@@ -14,18 +21,18 @@ start:
     mov ds, ax
 
     mov dx, offset title_msg
-    call _display_msg
+    call _print_dx_val
 
-    mov dx, offset shapes_lst_msg
-    call _display_msg
+    mov dx, offset lst_shapes_msg
+    call _print_dx_val
 
     call _exit
 
-_display_msg proc
+_print_dx_val proc
     mov ah, 09h
     int 21h
     ret
-_display_msg endp
+_print_dx_val endp
 
 _exit proc
     mov ah, 4Ch
