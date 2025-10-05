362bd7954fdc2eb53bf6fd20a9594cb87cdade0e
Miguel <mloranca703@gmail.com>
2025-10-05 13:32:14 -0600

    tried making a print arr function but barery worked using the si and di registers

--- assembly_8086/exercises/arr/arr.asm
+++ assembly_8086/exercises/arr/arr.asm
@@ -1,55 +1,36 @@
-ascii_char_offset equ 48
-
-.model small
-
 .stack 100h
 
 .data
-    d1 dw 123, 4, 6, 8
+    arr dw 1, 2, 3, 4, 5, 6
+
 .code
-
-main proc far
     mov ax, @data
-    mov ds, ax
+    mov s, ax
 
-    mov ax, d1
+    mov si, offset arr
+    mov bx, 6
+    call _print_arr
 
-    call print
-
-    mov ah, 4ch
+    mov ax, 4c00h
     int 21h
 
-main endp
+; Parameters: SI = array offset, BX = length
+_print_arr proc
+    xor cx, cx
+    print_loop:
+        cmp cx, bx
+        jge exit
 
-print proc
-    mov cx, 0
-    mov dx, 0
-
-    label1:
-        cmp ax, 0
-        je print1
-
-        mov bx, 10
-        div bx
-        push dx
-        inc cx
-
-        xor dx, dx
-        jmp label1
-    print1:
-        cmp cx, 0
-        je exit
-
-        pop dx
-        add dx, ascii_char_offset
-
-        mov ah, 02h
+        mov ah, 2
+        mov di, si
+        add di, cx
+        mov dl, [di]
+        add dl, '0'
         int 21h
 
-        dec cx
-        jmp print1
-
+        inc cx
+        jmp print_loop
     exit:
         ret
-print endp
-end main
+_print_arr endp
+
